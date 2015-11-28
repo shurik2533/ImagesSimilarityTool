36c349028f41cd898a09bce2222fc503723110d7
Aleksandr Dmitriev <shurik2533@ya.ru>
2015-11-28 18:30:17 +0300

    initial commit

---
+++ ImagesSimilarityTool/src/main/java/ru/s2533/similarity/ImagesSimilarity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.s2533.similarity;
+
+/**
+ *
+ * @author shurik2533
+ */
+public class ImagesSimilarity {
+  
+}
